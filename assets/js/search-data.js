1f6a02f8d9df640e102d0baec11977825b1ed75b
jayleenjiang <153397107+jayleenjiang@users.noreply.github.com>
2025-12-16 05:39:01 +0000

    Deploying to gh-pages from @ jayleenjiang/jayleenjiang.github.io@89570f4399f585c7cf1305be12e16049645a2052 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-ta",
+          title: "TA",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/TA/";
           },
         },{id: "dropdown-interests",
               title: "interests",
